b63ae4a7b92b8cf142772b343c4c37a2b7fe06cf
Vladislav <70939188+Lordan0001@users.noreply.github.com>
2023-03-05 16:24:05 +0300

    i hate websockets

---
+++ LAB_1.2/IISHandler1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace LAB_1._2
+{
+    public class IISHandler1 : IHttpHandler
+    {
+        WebSocket socket;
+        public bool IsReusable
+        {
+
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(WebSocketRequest);
+            }
+        }
+
+        private async Task WebSocketRequest(AspNetWebSocketContext context)
+        {
+            socket = context.WebSocket;
+            await Send(DateTime.Now.ToString("HH:mm:ss"));
+
+            while (socket.State == WebSocketState.Open)
+            {
+                Thread.Sleep(2000);
+                await Send(DateTime.Now.ToString("HH:mm:ss"));
+            }
+        }
+
+        private async Task Send(string s)
+        {
+            var sendbuffer = new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes(s));
+            await socket.SendAsync(sendbuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+}

65bb10ee0b3e6b4f7b22e40ba5d1f6277cd2f9b8
Vladislav <70939188+Lordan0001@users.noreply.github.com>
2023-03-13 04:12:10 +0300

    index - done, work on serialization

---
+++ LAB_2/Controllers/DictController.cs
@@ -0,0 +1,65 @@
+﻿using LAB_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LAB_2.Controllers
+{
+    public class DictController : Controller
+    {
+        // GET: Dict
+        public ActionResult Index()
+        {           
+            List<DictModel> dictModels = new List<DictModel>();
+            using(StreamReader r = new StreamReader(Server.MapPath("/DB/PhoneNumber.json")))
+            {
+                string json = r.ReadToEnd();
+                dictModels = JsonConvert.DeserializeObject<List<DictModel>>(json);
+                ViewBag.NumberList = dictModels;
+            
+            }
+     
+            ViewData["Head"] = "Привет мир!";
+            ViewBag.Second = "Hey cruel world!";
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSave(int Id,string Name,string Number)
+        {
+            DictModel newDictModel = new DictModel();
+            newDictModel.Id = Id;
+            newDictModel.Name = Name;
+            newDictModel.Number = Number;
+
+            List<DictModel> dictModels = new List<DictModel>();
+            using (StreamReader r = new StreamReader(Server.MapPath("/DB/PhoneNumber.json")))
+            {
+                string json = r.ReadToEnd();
+                dictModels = JsonConvert.DeserializeObject<List<DictModel>>(json);
+                dictModels.Add(newDictModel);
+                json = JsonConvert.SerializeObject(dictModels, Formatting.Indented);//TODO Serialize this into file beach
+                ViewBag.NumberList = dictModels;
+
+            }
+
+
+
+
+            return View("Index");
+        }
+
+
+
+
+    }
+}
---
+++ LAB_2/Models/DictModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB_2.Models
+{
+    public class DictModel 
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
